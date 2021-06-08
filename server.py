c93c8a54a2844b275dbb4bb5e133306101529111
Lena <helly.luchina@gmail.com>
2021-06-08 15:53:16 +0300

    cleaned up

--- server.py
+++ server.py
@@ -2,74 +2,49 @@ import argparse
 import json
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+USEDPORT = 4567
 
 class S(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
+	def _set_headers(self):
+		self.send_response(200)
+		self.send_header("Content-type", "text/html")
+		self.end_headers()
 
-    def _html(self, message):
-        """This just generates an HTML document that includes `message`
-        in the body. Override, or re-write this do do more interesting stuff.
+	def do_HEAD(self):
+		self._set_headers()
 
-        """
-        content = f"<html><body><h1>{message}</h1></body></html>"
-        return content.encode("utf8")  # NOTE: must return a bytes object!
+	def do_POST(self):
+		content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+		message = json.loads(self.rfile.read(content_length)) ## it is a dictionary
+		self._set_headers()
+		action = message["action"]	#closed
+		from_branch = message["pull_request"]["head"]["ref"]	# in staging (feature branch SD-873)
+		to_branch = message["pull_request"]["base"]["ref"]		# main (in staging)
+		print(action + ": " + from_branch + " -> " + to_branch)
 
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write(self._html("hi!"))
+def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=USEDPORT):
+	server_address = (addr, port)
+	httpd = server_class(server_address, handler_class)
 
-    def do_HEAD(self):
-        self._set_headers()
-
-    def do_POST(self):
-        # Doesn't do anything with posted data
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        # post_data = self.rfile.read(content_length) # <--- Gets the data itself
-        # self._set_headers()
-        # self.wfile.write("<html><body><h1>POST!</h1><pre>" + post_data + "</pre></body></html>")
-
-        message = json.loads(self.rfile.read(content_length)) ## it is a dictionary
-
-        # add a property to the object, just to mess with data
-        # message['received'] = 'ok'
-
-        # send the message back
-        self._set_headers()
-        msg = json.dumps(message)
-        action = message["action"]	#closed
-        from_branch = message["pull_request"]["head"]["ref"]	# in staging (feature branch SD-873)
-        to_branch = message["pull_request"]["base"]["ref"]		# main (in staging)
-        print(from_branch + " -> " + to_branch)
-        # print(msg)
-        # self.wfile.write("<html><body><h1>POST!</h1><pre>ololo</pre></body></html>")
-
-
-def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8000):
-    server_address = (addr, port)
-    httpd = server_class(server_address, handler_class)
-
-    print(f"Starting httpd server on {addr}:{port}")
-    httpd.serve_forever()
+	print(f"Starting httpd server on {addr}:{port}")
+	httpd.serve_forever()
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="Run a simple HTTP server")
-    parser.add_argument(
-        "-l",
-        "--listen",
-        default="localhost",
-        help="Specify the IP address on which the server listens",
-    )
-    parser.add_argument(
-        "-p",
-        "--port",
-        type=int,
-        default=4567,
-        help="Specify the port on which the server listens",
-    )
-    args = parser.parse_args()
-    run(addr=args.listen, port=args.port)
+	parser = argparse.ArgumentParser(description="Run a simple HTTP server")
+	parser.add_argument(
+		"-l",
+		"--listen",
+		default="localhost",
+		help="Specify the IP address on which the server listens",
+	)
+	parser.add_argument(
+		"-p",
+		"--port",
+		type=int,
+		default=USEDPORT,
+		help="Specify the port on which the server listens",
+	)
+	args = parser.parse_args()
+	run(addr=args.listen, port=args.port)
--- server.rb
+++ server.rb
@@ -2,8 +2,9 @@ require 'json'
 require 'sinatra'
 
 post '/payload' do
-  push = JSON.parse(request.body.read)
-  puts "\n\n #{push} \n\n"
+  puts "\n\n#{request.body.read}\n\n"
+  # push = JSON.parse(request.body.read)
+  # puts "\n\n #{push} \n\n"
   # from = push.pull_request.head.ref
   # to = push.pull_request.ref
   # puts "I got some JSON: #{push.inspect}"
